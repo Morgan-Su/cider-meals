7267b1c629fbe5ebad966269760859087c1f87c5
Morgan <captainsu101@gmail.com>
2025-08-26 20:11:05 -0400

    upload to testfligtht try 2

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -3,9 +3,15 @@ import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import SafeScreen from "@/components/SafeScreen";
 
+const CLERK_PUBLISHABLE_KEY =
+  "pk_test_ZXF1aXBwZWQtc3R1ZC03OC5jbGVyay5hY2NvdW50cy5kZXYk";
+
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache}>
+    <ClerkProvider
+      publishableKey={CLERK_PUBLISHABLE_KEY}
+      tokenCache={tokenCache}
+    >
       <SafeScreen>
         <Slot />
       </SafeScreen>
